c595f80a3f965ad7ad0165c22715f9ccdb2cc1c3
Madurai Corporation <itzsarank@gmail.com>
2022-08-16 10:37:52 +0530

    Router Component and Configure Completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import Home from './component/Home';
 import Navbar from './component/Navbar';
+import {Routes, Route} from 'react-router-dom';
+import Products from './component/Products';
 
 function App() {
   return (
-    <div> 
+    <> 
       <Navbar />
-      <Home />
-    </div>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/products" element={<Products/>} />
+      </Routes>
+    </>
   );
 }
 
